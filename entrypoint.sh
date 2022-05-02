033fe731449129c045b0b586cc93ef0285746168
ivanphdz <ivanphdz13@gmail.com>
2022-05-02 12:41:28 -0500

    feat(context): add context input that will allow to add context params to be used in the deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,11 @@
 
 EXTRA_ARGS=""
 if [ -n "${2}" ]; then
-   EXTRA_ARGS="--application=${2}"
+   EXTRA_ARGS+="--application=${2} "
+fi
+
+if [ -n "${3}" ]; then
+   EXTRA_ARGS+="--add-context=${3} "
 fi
 
 armory deploy start "${EXTRA_ARGS}" --file "${1}"
